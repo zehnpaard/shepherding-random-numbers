7482c0f7e67c5f968e1610ff20c4f3a64d015b98
zehnpaard <zehnpaard@googlemail.com>
2018-03-31 03:49:47 +0900

    Add sketch for special velocity

---
+++ src/shepherding_random_numbers/specspeed.clj
@@ -0,0 +1,56 @@
+(ns shepherding-random-numbers.memspeed
+  (:require [quil.core :as q]))
+
+(defn make-circle [x]
+  {:x x
+   :y 0
+   :dy 0
+   :sdy 0})
+
+(defn setup []
+  (q/frame-rate 60)
+  (q/color-mode :hsb)
+  (map make-circle (range -9 9.1 0.3)))
+
+
+(defn update-special-velocities [state]
+  (let [sdys (reductions + 0 (map :dy state))]
+    (map #(assoc %1 :sdy %2) state sdys)))
+
+(defn bound [n lower upper]
+  (-> n (max lower) (min upper)))
+
+(defn update-circle [circle]
+  (let [dy (+ (:dy circle)
+              (-> (rand 2) dec (/ 2000)))
+        y (bound (+ (:y circle) dy (:sdy circle)) 
+                 -1.5 1.5)]
+    (-> circle
+        (assoc :dy dy)
+        (assoc :y y))))
+
+(defn update-state [state]
+  (->> state
+       update-special-velocities
+       (map update-circle)))
+
+(defn draw-circle [circle]
+  (let [x (* (:x circle) 50)
+        y (* (:y circle) 150)]
+    (q/no-stroke)
+    (q/with-fill [0 0 0]
+      (q/ellipse x y 10 10))
+    (q/stroke 0 0 0 180)
+    (q/stroke-weight 2)
+    (q/line x y x (+ y
+                     (* (:dy circle) 1000) 
+                     (* (:sdy circle) 1000)))))
+
+(defn draw-state [state]
+  (q/background 255)
+  (q/with-translation [500 250]
+    (doall
+      (for [circle state]
+        (draw-circle circle)))))
+
+
