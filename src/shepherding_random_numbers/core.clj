94d67952d4091f16b4edec02aff131a3f5a91399
zehnpaard <zehnpaard@googlemail.com>
2018-03-31 01:23:52 +0900

    Add sketch for memory balls

--- src/shepherding_random_numbers/core.clj
+++ src/shepherding_random_numbers/core.clj
@@ -2,13 +2,14 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]
             [shepherding-random-numbers.singleball :as sb]
-            [shepherding-random-numbers.multiball :as mb]))
+            [shepherding-random-numbers.multiball :as mb]
+            [shepherding-random-numbers.memory :as mem]))
 
-(q/defsketch shepherding-random-numbers
+(q/defsketch shepherding-random-numemers
   :title "Single random circle"
   :size [1000 500]
-  :setup mb/setup
-  :update mb/update-state
-  :draw mb/draw-state
+  :setup mem/setup
+  :update mem/update-state
+  :draw mem/draw-state
   :features [:keep-on-top]
   :middleware [m/fun-mode])
---
+++ src/shepherding_random_numbers/memory.clj
@@ -0,0 +1,49 @@
+(ns shepherding-random-numbers.memory
+  (:require [quil.core :as q]))
+
+(defn make-circle [x]
+  {:t 0
+   :x x
+   :y 0
+   :dy 0
+   :ty 0})
+
+(defn setup []
+  (q/frame-rate 60)
+  (q/color-mode :hsb)
+  (map make-circle (range -9 9.1 0.3)))
+
+(defn set-new-target [circle]
+  (let [ty (-> (rand 2) dec (/ 5) (+ (:y circle)))
+        dy (-> circle :y (- ty) (/ -2))]
+    (-> circle
+        (assoc :t 2)
+        (assoc :ty ty))))
+
+(defn update-circle [circle]
+  (if (zero? (:t circle))
+    (set-new-target circle)
+    (-> circle
+        (update :t dec)
+        (assoc :dy (-> circle :ty (- (:y circle)) (/ 30)))
+        (update :y #(+ % (:dy circle))))))
+
+(defn update-state [state]
+  (map update-circle state))
+
+(defn draw-circle [circle]
+  (q/no-stroke)
+  (q/with-fill [0 0 0 180]
+    (q/ellipse 
+      (-> circle :x (* 50)) 
+      (-> circle :y (* 150)) 
+      10 10)))
+
+(defn draw-state [state]
+  (q/background 255)
+  (q/with-translation [500 250]
+    (doall
+      (for [circle state]
+        (draw-circle circle)))))
+
+
